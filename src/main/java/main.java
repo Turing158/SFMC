d31caf58350779abc52d23cff3b6ceeb9e39a8ef
Turing158 <15818961209@163.com>
2023-11-07 01:05:01 +0800

    压缩背景

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-
 public class main extends Application {
     public static void main(String[] args) {
         launch(args);
